a6d150ba432ee5fd0ac5d9a0dbd517ac26100834
marfanr <mohammadarfan45@gmail.com>
2023-12-07 08:20:15 +0700

    feat: init

---
+++ runge_kutta.py
@@ -0,0 +1,11 @@
+class RungeKutta:
+    def __init__(self, df, xout, x0, y0, h):
+        pass
+
+def main():
+    x = "2x^2+3y^2+4xy"
+    rk =  RungeKutta(x, 1.2, 1, 1, 0,2)
+    pass
+
+if __name__ == '__main__':
+    main()
